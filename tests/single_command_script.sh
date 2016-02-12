e5e8fa61bead029f7d92142255018bdd57217366
Jamie Alaniz <jalan003@hammer.cs.ucr.edu>
2016-02-12 14:29:21 -0800

    Added script

---
+++ tests/single_command_script.sh
@@ -0,0 +1,24 @@
+Script started on Fri 12 Feb 2016 02:23:50 PM PST
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[?1034h[jalan003@hammer src]$ ./a.out
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/src$ cd ..
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell$ cd tests
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/tests$ ./single_o command.sh
+commented_command.sh  exit.sh  single_command.sh
+.   a.out  LICENSE   .main.cpp.swp  README.md  tests
+..  .git   main.cpp  makefile	    src
+testing
+a.out  LICENSE	main.cpp  makefile  README.md  src  temp  tests
+/home/csmajs/jalan003/CS100/Assignment2/rshell
+Fri Feb 12 14:24:19 PST 2016
+hammer.cs.ucr.edu
+jalan003
+rm: cannot remove `temp': Is a directory
+
+real	0m0.000s
+user	0m0.000s
+sys	0m0.000s
+jalan003@hammer.cs.ucr.edu /home/csmajs/jalan003/CS100/Assignment2/rshell/tests$ exit
+]0;jalan003@hammer:~/CS100/Assignment2/rshell/src[jalan003@hammer src]$ exit
+exit
+
+Script done on Fri 12 Feb 2016 02:24:23 PM PST
