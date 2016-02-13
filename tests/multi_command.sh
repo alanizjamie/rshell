7218792e67d52f7c09b06ac09b265f02b992c98d
alanizjamie <jalan003@ucr.edu>
2016-02-12 16:21:28 -0800

    Create multi_command.sh
    added multi_command.sh

---
+++ tests/multi_command.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ls && echo moose
+ls || echo moose 
+echo moose; echo dov;
+echo den; echo dov && ls
+cd .. || echo didn't work
+echo fist; echo second && echo third && echo fourth
+echo first || echo second && echo third
+date && time && pwd
+echo moose; cd .. || echo nope
+&& || && 
+|||
+exit
